0574298793ff0bf0129404a4b89d723648a69986
Alex Kipper <kippera27@gmail.com>
2024-07-05 19:38:10 +0300

    added some basic vs code inits for me

--- src/middlewares/logger.ts
+++ src/middlewares/logger.ts
@@ -1,10 +1,6 @@
 import { NextFunction, Request, RequestHandler, Response } from 'express';
 
-export const logger: RequestHandler = (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
+export const logger: RequestHandler = (req: Request, res: Response, next: NextFunction) => {
   console.log(`${req.method} ${req.path}`);
   next();
 };
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,16 @@
+import mongoose from 'mongoose';
 import app from './app';
+
 const PORT = 3000;
 
-
-
-
-
-
-
+mongoose
+  .connect('mongodb://127.0.0.1:27017/testdb', {})
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Failed to connect to MongoDB', err);
+  });
 
 app.listen(PORT, () => {
   console.log(`listening on port: ${PORT}`);
